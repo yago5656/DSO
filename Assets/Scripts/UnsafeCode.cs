a753943d3787318636202a99de4253c3454d9c59
yago5656 <yago.gon2007@gmail.com>
2022-10-18 16:35:33 -0300

    first commit

---
+++ Assets/Scripts/UnsafeCode.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+public static unsafe class UnsafeCode
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T ByteArrayToStructure<T>(byte[] data)
+    {
+        fixed (byte* ptr = data)
+        {
+            return Marshal.PtrToStructure<T>((IntPtr)ptr);
+        }
+    }
+
+    // same as memcpy in C
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void* Memcpy(void* dest, void* src, int len)
+    {
+        sbyte* d = (sbyte*)dest;
+        sbyte* s = (sbyte*)src;
+        while (len-- > 0)
+            *d++ = *s++;
+        return dest;
+    }
+
+    // since we cant do ptr + offset like we can do in C,
+    // we have to come out with another way
+    public static byte[] SubArray(byte[] array, int offset, int length)
+    {
+        byte[] result = new byte[length];
+        fixed (byte* ptr = array, ptrResult = result)
+        {
+            byte* newPtr = ptr + offset;
+            Memcpy(ptrResult, newPtr, length);
+        }
+        return result;
+    }
+}
